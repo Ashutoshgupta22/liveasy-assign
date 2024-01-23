ce808f49f3f704ab3955f5cf7a13570094e9a245
Ashutosh Gupta <68068357+Ashutoshgupta22@users.noreply.github.com>
2024-01-23 17:27:31 +0530

    initial commit

---
+++ src/main/java/com/example/liveasylogisticsassign/LiveasyLogisticsAssignApplication.java
@@ -0,0 +1,13 @@
+package com.example.liveasylogisticsassign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveasyLogisticsAssignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LiveasyLogisticsAssignApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/liveasylogisticsassign/LiveasyLogisticsAssignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.liveasylogisticsassign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveasyLogisticsAssignApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
